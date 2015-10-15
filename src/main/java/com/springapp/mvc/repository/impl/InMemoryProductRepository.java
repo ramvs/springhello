2260aa08344a115a12d6aa44189c236523486271
Krystian <krystian.kawa1@gmail.com>
2015-10-15 20:00:05 +0200

    added image for all products and for single one

--- src/main/java/com/springapp/mvc/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/springapp/mvc/repository/impl/InMemoryProductRepository.java
@@ -15,18 +15,18 @@ public class InMemoryProductRepository implements ProductRepository {
     private List<Product> listOfProducts = new ArrayList<Product>();
 
     public InMemoryProductRepository() {
-        Product iphone = new Product("P1234", "iPhone 5s", new BigDecimal(500));
+        Product iphone = new Product("1", "iPhone 5s", new BigDecimal(500));
         iphone.setDescription("Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
         iphone.setCategory("Smart Phone");
         iphone.setManufacturer("Apple");
         iphone.setUnitsInStock(1000);
-        Product laptop_dell = new Product("P1235", "Dell Inspiron",
+        Product laptop_dell = new Product("2", "Dell Inspiron",
                 new BigDecimal(700));
         laptop_dell.setDescription("Dell Inspiron 14-inch Laptop (Black)with 3rd Generation Intel Core processors");
         laptop_dell.setCategory("Laptop");
         laptop_dell.setManufacturer("Dell");
         laptop_dell.setUnitsInStock(1000);
-        Product tablet_Nexus = new Product("P1236", "Nexus 7", new
+        Product tablet_Nexus = new Product("3", "Nexus 7", new
                 BigDecimal(300));
         tablet_Nexus.setDescription("Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon™ S4 Pro processor");
         tablet_Nexus.setCategory("Tablet");
--- src/main/webapp/WEB-INF/pages/product.jsp
+++ src/main/webapp/WEB-INF/pages/product.jsp
@@ -32,6 +32,10 @@
 <section class="container">
     <div class="row">
         <div class="col-md-5">
+            <img src="<c:url value="/resource/images/${product.productId}.png">
+</c:url>" alt="image" style="width:100%"/>
+        </div>
+        <div class="col-md-5">
             <h3>${product.name}</h3>
 
             <p>${product.description}</p>
@@ -57,6 +61,7 @@
             <a href="<spring:url value="/products" />" class="btn btn-default">
                 <span class="glyphicon-hand-left glyphicon"></span> back
             </a>
+
             <p>
                 <a href="#" class="btn btn-warning btn-large"> <span
                         class="glyphicon-shopping-cart glyphicon"></span> Order Now
